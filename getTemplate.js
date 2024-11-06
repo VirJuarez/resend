8f1c0166154b2876e645d11ba53b3ceca4f7be68
Virignia <virginia.juarez1996@gmail.com>
2024-11-06 11:46:50 -0300

    Backend - envio de email

---
+++ getTemplate.js
@@ -0,0 +1,22 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function getEmailTemplate(templateName, variables) {
+  try {
+    let template = await fs.readFile(path.join(
+      __dirname, `${templateName}.html`),
+      'utf8'
+    );
+
+    for (let key in variables) {
+        template = template.replace(`{${key}}`, variables[key]);
+      }
+    
+    return template;
+  } catch (error) {
+    console.error('Error loading email template:', error);
+    throw error;
+  }
+}
+
+module.exports = { getEmailTemplate };
